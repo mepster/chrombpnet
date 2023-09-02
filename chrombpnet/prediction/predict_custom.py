d8e9e2029eb7dc4d2ffdd97cc2b7a0d1dbe5868f
Michael E. Palmer <mep@mepalmer.net>
2023-09-02 00:33:36 -0700

    naming of output sequences. multigpu. workaround for tf strategy closing bug

--- chrombpnet/prediction/predict_custom.py
+++ chrombpnet/prediction/predict_custom.py
@@ -3,6 +3,7 @@ import pandas as pd
 from tensorflow.keras.utils import get_custom_objects
 from tensorflow.keras.models import load_model
 import tensorflow as tf
+from chrombpnet.helpers.misc import get_strategy
 #import chrombpnet.prediction.bigwig_helper as bigwig_helper
 import chrombpnet.training.utils.losses as losses
 import chrombpnet.training.utils.one_hot as one_hot
@@ -11,31 +12,22 @@ import h5py
 ALLVAR_SCHEMA = ["chr", "start", "end", "species", "peak_name", "6", "7", "8", "9", "10", "sequence"]
 SINGLEVAR_SCHEMA = ["chr_hum", "start_hum", "end_hum", "SNP_hum_chimp", "species", "chr_chimp", "start_chimp", "end_chimp", "peak_name", "10", "11", "12", "13", "14", "sequence"]
 
-def write_predictions_h5py(output_prefix, profile, logcts, coords):
+def write_predictions_h5py(output_prefix, profile, logcts, names):
     # open h5 file for writing predictions
     output_h5_fname = "{}_predictions.h5".format(output_prefix)
     h5_file = h5py.File(output_h5_fname, "w")
+
     # create groups
-    coord_group = h5_file.create_group("coords")
     pred_group = h5_file.create_group("predictions")
 
-    num_examples=len(coords)
-
-    coords_chrom_data =  [str(coords[i][0]) for i in range(num_examples)]
-    coords_start_data =  [int(coords[i][1]) for i in range(num_examples)]
-
+    # create the "names"  dataset
+    names_data =  [str(name) for name in names]
     dt = h5py.special_dtype(vlen=str)
 
-    # create the "coords" group datasets
-    coords_chrom_dset = coord_group.create_dataset(
-        "coords_chrom",
-        data=np.array(coords_chrom_data, dtype=dt),
+    names_dset = h5_file.create_dataset(
+        "names",
+        data=np.array(names_data, dtype=dt),
         dtype=dt,
-        compression="gzip")
-    coords_start_dset = coord_group.create_dataset(
-        "coords_start",
-        data=coords_start_data,
-        dtype=int,
         compression="gzip")
 
     # create the "predictions" group datasets
@@ -67,14 +59,10 @@ def load_model_wrapper(model_hdf5):
     return model
 
 def main(args):
-
-    # load model
-    model_chrombpnet_nb = load_model_wrapper(model_hdf5=args.chrombpnet_model_nb)
-    inputlen = int(model_chrombpnet_nb.input_shape[1])
-    outputlen = int(model_chrombpnet_nb.output_shape[0][1])
+    # get tf strategy to either run on single, or multiple GPUs
+    strategy = get_strategy(args)
 
     # load data
-
     regions_df = pd.read_csv(args.input_bed_file, sep='\t', names=ALLVAR_SCHEMA) # have to add an argument to choose the schema
     print(regions_df.head())
 
@@ -82,8 +70,16 @@ def main(args):
     print(seqs)
     one_hot_seqs = one_hot.dna_to_one_hot(seqs)
     print(one_hot_seqs)
+
+    with strategy.scope():
+        # load model
+        model_chrombpnet_nb = load_model_wrapper(model_hdf5=args.chrombpnet_model_nb)
+        inputlen = int(model_chrombpnet_nb.input_shape[1])
+        outputlen = int(model_chrombpnet_nb.output_shape[0][1])
+
     assert(len(seqs[0]) == inputlen)
 
+    # predict
     pred_logits_wo_bias, pred_logcts_wo_bias = model_chrombpnet_nb.predict([one_hot_seqs],
                                       batch_size = args.batch_size,
                                       verbose=True)
@@ -100,9 +96,17 @@ def main(args):
     profile_probs_predictions = softmax(pred_logits_wo_bias)
     counts_sum_predictions = np.squeeze(pred_logcts_wo_bias)
 
-    coordinates = regions_df[['chr', 'start']].values.tolist()
-    print(coordinates)
-    write_predictions_h5py(args.output_prefix + "_chrombpnet_nobias", profile_probs_predictions, counts_sum_predictions, coordinates)
+    # get the name for each row
+    fields = regions_df.iloc[:, 0:5]
+    print(fields)
+    names = ["/".join(map(str,x)) for x in fields.values.tolist()]
+
+    # write named predictions
+    write_predictions_h5py(args.output_prefix + "_chrombpnet_nobias", profile_probs_predictions, counts_sum_predictions, names)
+
+    # workaround to explicitly close strategy. https://github.com/tensorflow/tensorflow/issues/50487
+    import atexit
+    atexit.register(strategy._extended._collective_ops._pool.close)  # type: ignore
 
 
 if __name__=="__main__":
